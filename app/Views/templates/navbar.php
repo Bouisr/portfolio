21dd44ff1cf47950882970e54224b7df73e93da9
Romain <r.bouis14@gmail.com>
2022-03-23 18:32:44 +0100

    Modification des routes et navbar dynamique

--- app/Views/templates/navbar.php
+++ app/Views/templates/navbar.php
@@ -6,12 +6,17 @@
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fas fa-bars"></i>
+                    
                 </button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
+
+                // Barre de naviguation à faire
+                    <?php if (strpos(current_url(), '') OR strpos(current_url(), 'home/index') && (session()->get('isLoggedIn') == true)): ?>
                         <li class="nav-item"><a class="nav-link" href="#about">À propos</a></li>
                         <li class="nav-item"><a class="nav-link" href="#projects">Projets</a></li>
                         <li class="nav-item"><a class="nav-link" href="#contact">Contactez-moi</a></li>
+                        <?php endif ?>
                         <?php if (session()->get('isLoggedIn')): ?>
 
                             <li class="nav-item"><?php echo anchor('user/signout', 'Déconnexion', 'class="nav-link"') ?></li>
@@ -23,6 +28,11 @@
                                 <?php endif ?>
 
                         <?php endif ?>
+                    <?php if (strpos(current_url(), 'user/singin')): ?>
+                        <li class="nav-item"><a class="nav-link" href="#signin">S'indentifier</a></li>
+                        <li class="nav-item"><?php echo anchor('home/index', 'Retour à la page d\'accueil', 'class="nav-link"') ?></li>
+                        
+                    <?php endif ?>
                             
                     </ul>
                 </div>
